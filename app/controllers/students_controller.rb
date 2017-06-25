b0479c37556cd3d4e6d839463ec177d1ad653430
Latrena Weatherspoon <llweatherspo@cps.edu>
2017-06-25 20:23:14 +0000

    fonts

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,10 @@
 class StudentsController < ApplicationController
+    def latrena
+        @full_name="Latrena Weatherspoon"
+        @bio="I am 17 years old. I live on the south side of Chicago."
+        @restaurant="My favorite restaurant is Chili's"
+        @tvshow="My favorite show is Atlanta"
+        
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+    get 'latrena', to: "students#latrena"
 # Follow this format for your route: get '/harsha', to: 'students#harsha'
 end
